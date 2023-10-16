2d44bec267c4a8ca5f14ce758c4b1e05e5858418
demony33 <93370520+demony33@users.noreply.github.com>
2023-10-16 01:08:54 -0500

    Update passvie_scan.sh

--- passvie_scan.sh
+++ passvie_scan.sh
@@ -13,8 +13,7 @@ fi
 
 ### PERFROM SCAN ###
 
-printf "\x1b[32m ---> [ Starting scan against domains:]\\x1b[0m\n" ;
-
+printf "\x1b[32m ---> [ DNS Enumeration -Find Subdomains:]\\x1b[0m\n" ;
 ## DNS Enumeration -Find Subdomains
 amass enum -passive -norecursive -noalts -df "$scope/domains.txt" | anew "$outpath/subs.txt"
 
@@ -22,17 +21,21 @@ subfinder -dL "$scope/domains.txt" -all | anew "$outpath/subs.txt"
 
 shuffledns -l "$scope/domains.txt" -w "$wordlists/2m-subdomains.txt" -r "$wordlists/resolvers-trusted.txt" | anew "$outpath/subs.txt"
 
+printf "\x1b[32m ---> [ DNS Resolution - Resolver Discovered Subdomains:]\\x1b[0m\n" ;
 ## DNS Resolution - Resolver Discovered Subdomains
 puredns resolve "$outpath/subs.txt" -r "$wordlists/resolvers-trusted.txt" -w "$outpath/resolved.txt" | wc -l
 
 dnsx -l "$outpath/resolved.txt" -json -o "$outpath/dns.json" | jq -r '.a?[]?' | anew "$outpath/ips.txt" | wc -l 
 
+printf "\x1b[32m ---> [ Port Scanning & HTTP Server Discovery:]\\x1b[0m\n" ;
 ## Port Scanning & HTTP Server Discovery
 naabu -l "$outpath/resolved.txt" -pf port_list.txt | anew "$outpath/port.txt"
 
 httpx -l "$outpath/port.txt" -timeout 10 -cl -wc -fr -title -sc | anew "$outpath/httpxscan.txt"
 
+printf "\x1b[32m ---> [ Webscanning - nuclei:]\\x1b[0m\n" ;
 nuclei -l "$outpath/port.txt" -etags dns,ssl -es info -o "$outpath/report.txt"
 
+printf "\x1b[32m ---> [ Have been sent to discord!]\\x1b[0m\n" ;
 #notify
 notify -data "$outpath/report.txt" -bulk
