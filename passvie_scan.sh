4b311048989a42663d886dcdb2523105846d74ca
demony33 <93370520+demony33@users.noreply.github.com>
2023-11-22 10:44:21 +0800

    Update passvie_scan.sh

--- passvie_scan.sh
+++ passvie_scan.sh
@@ -25,17 +25,17 @@ printf "\x1b[32m ---> [ DNS Resolution - Resolver Discovered Subdomains:]\\x1b[0
 ## DNS Resolution - Resolver Discovered Subdomains
 puredns resolve "$outpath/subs.txt" -r "$wordlists/resolvers-trusted.txt" -w "$outpath/resolved.txt" | wc -l
 
-dnsx -l "$outpath/resolved.txt" -json -o "$outpath/dns.json" | jq -r '.a?[]?' | anew "$outpath/ips.txt" | wc -l 
+#dnsx -l "$outpath/resolved.txt" -json -o "$outpath/dns.json" | jq -r '.a?[]?' | anew "$outpath/ips.txt" | wc -l 
 
 printf "\x1b[32m ---> [ Port Scanning & HTTP Server Discovery:]\\x1b[0m\n" ;
 ## Port Scanning & HTTP Server Discovery
-naabu -l "$outpath/resolved.txt" -pf port_list.txt | anew "$outpath/port.txt"
+#naabu -l "$outpath/resolved.txt" -pf port_list.txt | anew "$outpath/port.txt"
 
 httpx -l "$outpath/port.txt" -timeout 10 -cl -wc -fr -title -sc | anew "$outpath/httpxscan.txt"
 
-printf "\x1b[32m ---> [ Webscanning - nuclei:]\\x1b[0m\n" ;
+#printf "\x1b[32m ---> [ Webscanning - nuclei:]\\x1b[0m\n" ;
 nuclei -l "$outpath/port.txt" -etags dns,ssl -es info -o "$outpath/report.txt"
 
-printf "\x1b[32m ---> [ Have been sent to discord!]\\x1b[0m\n" ;
+#printf "\x1b[32m ---> [ Have been sent to discord!]\\x1b[0m\n" ;
 #notify
 notify -data "$outpath/report.txt" -bulk
